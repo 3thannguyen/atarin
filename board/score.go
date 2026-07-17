d067309b3c377257aab37f3323a53f8d46d50228
3thannguyen <orangevinh@gmail.com>
2026-07-17 13:41:45 -0400

    added tests for board, new check for whether isSimpleEye, renamed gtp.go

--- board/score.go
+++ board/score.go
@@ -54,3 +54,37 @@ func (b *Board) Score() int {
 	// returns score in this format before komi. komi can be configured later
 	return black - white
 }
+
+/* an important aspect of go is checking if an eye is real or false;
+i still have yet to think of a way to properly implement it, but apparently a practical rule
+is that an eye is real if the opponent has <= 1 stone in the diags if the eye is in center,
+none if the eye is on the edge. having a check on whether we have a simply eye would be helpful
+for the agent's playmaking as well (so that it doesn't play in its own eye)
+*/
+
+func (b *Board) isSimplyEye(p int, c Color) bool {
+	var nb [4]int
+	b.neighbors(p, &nb)
+	// checking if all neighbors are our color/edge
+	for _, q := range nb {
+		if b.points[q] != c && b.points[q] != Edge {
+			return false
+		}
+	}
+	n := b.Size + 1
+	diags := [4]int{p - n - 1, p - n + 1, p + n - 1, p + n + 1}
+	enemy, edge := 0, 0
+	opp := c.Opponent()
+	for _, d := range diags {
+		switch b.points[d] {
+		case opp:
+			enemy++
+		case Edge:
+			edge++
+		}
+	}
+	if edge > 0 {
+		return enemy == 0
+	}
+	return enemy <= 1
+}
